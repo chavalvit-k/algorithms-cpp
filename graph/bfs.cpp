063088110a76838ccfaf0d475f9ea74b9b512c9d
p4ssa <chokunsmurf2@gmail.com>
2025-01-25 21:18:21 +0700

    add: bfs + dfs

---
+++ graph/bfs.cpp
@@ -0,0 +1,40 @@
+/*
+Author: passa
+Subject: Breadth First Search
+Language: C++ 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MXN 100100
+
+int n,m,u,v;
+int visited[MXN];
+vector<int> g[MXN];
+queue<int> q;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0 ; i < m ; i++) {
+        cin >> u >> v;
+        g[u].push_back(v); g[v].push_back(u);
+    }
+
+    q.push(1);
+    while (!q.empty()) {
+        u = q.front();
+        cout << u << endl;
+        visited[u] = true;
+        q.pop();
+        for (auto v: g[u]) {
+            if (!visited[v]) q.push(v);
+        }
+    }
+
+    return 0;
+}
---
+++ graph/dfs.cpp
@@ -0,0 +1,40 @@
+/*
+Author: passa
+Subject: Depth First Search
+Language: C++ 
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MXN 100100
+
+int n,m,u,v;
+int visited[MXN];
+vector<int> g[MXN];
+stack<int> s;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    for (int i = 0 ; i < m ; i++) {
+        cin >> u >> v;
+        g[u].push_back(v); g[v].push_back(u);
+    }
+
+    s.push(1);
+    while (!s.empty()) {
+        u = s.top();
+        cout << u << endl;
+        visited[u] = true;
+        s.pop();
+        for (auto v: g[u]) {
+            if (!visited[v]) s.push(v);
+        }
+    }
+    
+    return 0;
+}
